d6b3977ac2d13519ffc03422aea559004d606c1d
Sonal4y <sonal4y@gmail.com>
2023-11-20 16:20:25 +0530

    done2

---
+++ Product.java
@@ -0,0 +1,15 @@
+package Day1;
+//wap for product of two number
+public class Product {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//local variable
+		int a = 22;
+		int b = 12;
+		int c = a*b;
+	
+		System.out.append("Product = "+c);
+	}
+
+}
